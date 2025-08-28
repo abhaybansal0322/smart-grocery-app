e46ebd17028dda55856abdc1c992583e6c5b8189
Abhay Bansal <abhaybansal19000@gmail.com>
2025-08-28 15:28:34 +0530

    Deployment ready

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' }
+    ]
+  },
 };
 
 module.exports = nextConfig;
